350e8546c4809e0126fcdf96ff5e02daba668614
SomeyaGinji <cist.b220.g.someya@gmail.com>
2023-11-18 13:15:47 +0900

    DB構築とDBへのアクセスを可能にした

---
+++ src/main/java/org/vaadin/example/service/SnowService.java
@@ -0,0 +1,26 @@
+package org.vaadin.example.service;
+
+import org.springframework.stereotype.Service;
+import org.vaadin.example.data.ShovelingPlace;
+import org.vaadin.example.repository.SnowRepository;
+
+import java.util.List;
+
+@Service
+public class SnowService {
+
+    private final SnowRepository snowRepository;
+
+    public SnowService(SnowRepository snowRepository){
+        this.snowRepository = snowRepository;
+    }
+
+    public void insertShovelingPlace(ShovelingPlace shovelingPlace){
+        snowRepository.insertShovelingPlace(shovelingPlace);
+    }
+
+    public List<ShovelingPlace> getShovelingPlaceList(){
+        return snowRepository.getShovelingPlaceList();
+    }
+
+}
